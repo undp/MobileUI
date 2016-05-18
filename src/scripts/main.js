7e3420969e7f0ef59cfb78e9c707f29e5e13baee
Sachin Kamble <sachin@clistesoft.com>
2016-05-18 16:02:26 +0530

    edited: Misc changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -122,18 +122,5 @@
     $('[data-popupmenu]').click(function(){
       $('#contriesPop').addClass('hide');
     });
-
-    var activePath = location.pathname;
-    switch(activePath){
-      case '/our-work.html':
-        $('.list-main-nav').find('.our-work').addClass('active');
-        break;
-      case '/sustainable-development-goals.html':
-        $('.list-main-nav').find('.development-goals').addClass('active');
-        break;
-      case '/blog.html':
-        $('.list-main-nav').find('.blog').addClass('active');
-        break;
-    }
   });
 })(window, document, jQuery);
