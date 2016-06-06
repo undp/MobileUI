51b51c8e45965e8c2a141c4cd473b00c2d426d48
ajinkya <ajinkya@clistesoft.com>
2016-06-06 16:41:22 +0530

    rtl in progress

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -199,33 +199,7 @@
     });
 
 
-//scrollable tabs
-    $('.slick-tabs').slick({
-      //setting-name: setting-value
-      slidesToShow: 4,
-      infinite: false,
-      swipeToSlide: true,
-      responsive: [
-        {
-          breakpoint: 768,
-          settings: {
-            slidesToShow: 4
-          }
-        },
-        {
-          breakpoint: 667,
-          settings: {
-            slidesToShow: 3
-          }
-        },
-        {
-          breakpoint: 568,
-          settings: {
-            slidesToShow: 2
-          }
-        }
-      ]
-  });
+
 
 
   });
